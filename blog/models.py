c60805bba8a18f34106ec7383bcf3bcadfa44db5
Moonsur <monsur.cs.it@gmail.com>
2020-12-18 22:21:36 +0600

    Blog apps up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.timezone import datetime
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(default=datetime.now, blank=True)
+    body = models.TextField(default='')
+    image = models.ImageField(upload_to='media/')
