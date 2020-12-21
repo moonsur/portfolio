be88d548387b40c0ae6ad5b71ff5f3d76ef9d57c
Moonsur <monsur.cs.it@gmail.com>
2020-12-21 16:59:18 +0600

    Blog apps up and running

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField(default=datetime.now, blank=True)
     body = models.TextField(default='')
     image = models.ImageField(upload_to='media/')
+
+    def __str__(self):
+        return self.title
+
+    def summery(self):
+        return self.body[:100]
+
+    def pub_date_short(self):
+        return self.pub_date.strftime("%b %d %Y")
